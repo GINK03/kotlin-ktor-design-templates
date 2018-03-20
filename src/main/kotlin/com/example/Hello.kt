c7753eb27d9f6732d01b76f9ed83a5b22745f5eb
Your Name <gim.kobayashi@gmail.com>
2018-03-20 15:53:39 +0900

    support json in netty

--- src/main/kotlin/com/example/Hello.kt
+++ src/main/kotlin/com/example/Hello.kt
@@ -7,8 +7,10 @@ import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.server.engine.*
 import io.ktor.server.jetty.*
+import io.ktor.gson.*
 
 import java.io.File
+import java.text.*
 
 class JsonResponse(val data: Any)
 data class Model(val name: String, val items: List<Item>)
@@ -17,9 +19,29 @@ data class Item(val key: String, val value: String)
 fun Application.module() {
   install(DefaultHeaders)
   install(CallLogging)
+  install(ContentNegotiation) {
+    gson {
+      setDateFormat(DateFormat.LONG)
+      setPrettyPrinting()
+    }
+  }
+  val model = Model("root", listOf(Item("A", "Apache"), Item("B", "Bing")))
   install(Routing) {
-    get("/") {
+    get("/hello") {
       call.respondText("Hello, World!")
+    }
+    get("/bye") {
+      call.respondText("Bye")
+    }
+    get("/v1") {
+      call.respond(model)
+    }
+    get("/v1/item/{key}") {
+      val item = model.items.firstOrNull { it.key == call.parameters["key"] }
+      if (item == null)
+        call.respond(HttpStatusCode.NotFound)
+      else
+        call.respond(item)
     }
   }
 }
